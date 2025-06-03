8baad71b2680921998f39eb3e644a988b2410492
Vlad <vla9ik2020@gmail.com>
2025-06-03 13:44:28 +0200

    Updates

--- js/dev/app.js
+++ js/dev/app.js
@@ -208,6 +208,166 @@ class DynamicAdapt {
 if (document.querySelector("[data-fls-dynamic]")) {
   window.addEventListener("load", () => new DynamicAdapt());
 }
+function digitsCounter() {
+  function digitsCountersInit(digitsCountersItems) {
+    let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-fls-digcounter]");
+    if (digitsCounters.length) {
+      digitsCounters.forEach((digitsCounter2) => {
+        if (digitsCounter2.hasAttribute("data-fls-digcounter-go")) return;
+        digitsCounter2.setAttribute("data-fls-digcounter-go", "");
+        digitsCounter2.dataset.flsDigcounter = digitsCounter2.innerHTML;
+        digitsCounter2.innerHTML = `0`;
+        digitsCountersAnimate(digitsCounter2);
+      });
+    }
+  }
+  function digitsCountersAnimate(digitsCounter2) {
+    let startTimestamp = null;
+    const duration = parseFloat(digitsCounter2.dataset.flsDigcounterSpeed) ? parseFloat(digitsCounter2.dataset.flsDigcounterSpeed) : 1e3;
+    const startValue = parseFloat(digitsCounter2.dataset.flsDigcounter);
+    const format = digitsCounter2.dataset.flsDigcounterFormat ? digitsCounter2.dataset.flsDigcounterFormat : " ";
+    const startPosition = 0;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      const value = Math.floor(progress * (startPosition + startValue));
+      digitsCounter2.innerHTML = typeof digitsCounter2.dataset.flsDigcounterFormat !== "undefined" ? getDigFormat(value, format) : value;
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      } else {
+        digitsCounter2.removeAttribute("data-fls-digcounter-go");
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  function digitsCounterAction(e) {
+    const entry = e.detail.entry;
+    const targetElement = entry.target;
+    if (targetElement.querySelectorAll("[data-fls-digcounter]").length && entry.isIntersecting) {
+      digitsCountersInit(targetElement.querySelectorAll("[data-fls-digcounter]"));
+    }
+  }
+  document.addEventListener("watcherCallback", digitsCounterAction);
+}
+document.querySelector("[data-fls-digcounter]") ? window.addEventListener("load", digitsCounter) : null;
+class ScrollWatcher {
+  constructor(props) {
+    let defaultConfig = {
+      logging: true
+    };
+    this.config = Object.assign(defaultConfig, props);
+    this.observer;
+    !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
+  }
+  // Оновлюємо конструктор
+  scrollWatcherUpdate() {
+    this.scrollWatcherRun();
+  }
+  // Запускаємо конструктор
+  scrollWatcherRun() {
+    document.documentElement.classList.add("watcher");
+    this.scrollWatcherConstructor(document.querySelectorAll("[data-fls-watcher]"));
+  }
+  // Конструктор спостерігачів
+  scrollWatcherConstructor(items) {
+    if (items.length) {
+      let uniqParams = uniqArray(Array.from(items).map(function(item) {
+        if (item.dataset.flsWatcher === "navigator" && !item.dataset.flsWatcherThreshold) {
+          let valueOfThreshold;
+          if (item.clientHeight > 2) {
+            valueOfThreshold = window.innerHeight / 2 / (item.clientHeight - 1);
+            if (valueOfThreshold > 1) {
+              valueOfThreshold = 1;
+            }
+          } else {
+            valueOfThreshold = 1;
+          }
+          item.setAttribute(
+            "data-fls-watcher-threshold",
+            valueOfThreshold.toFixed(2)
+          );
+        }
+        return `${item.dataset.flsWatcherRoot ? item.dataset.flsWatcherRoot : null}|${item.dataset.flsWatcherMargin ? item.dataset.flsWatcherMargin : "0px"}|${item.dataset.flsWatcherThreshold ? item.dataset.flsWatcherThreshold : 0}`;
+      }));
+      uniqParams.forEach((uniqParam) => {
+        let uniqParamArray = uniqParam.split("|");
+        let paramsWatch = {
+          root: uniqParamArray[0],
+          margin: uniqParamArray[1],
+          threshold: uniqParamArray[2]
+        };
+        let groupItems = Array.from(items).filter(function(item) {
+          let watchRoot = item.dataset.flsWatcherRoot ? item.dataset.flsWatcherRoot : null;
+          let watchMargin = item.dataset.flsWatcherMargin ? item.dataset.flsWatcherMargin : "0px";
+          let watchThreshold = item.dataset.flsWatcherThreshold ? item.dataset.flsWatcherThreshold : 0;
+          if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) {
+            return item;
+          }
+        });
+        let configWatcher = this.getScrollWatcherConfig(paramsWatch);
+        this.scrollWatcherInit(groupItems, configWatcher);
+      });
+    }
+  }
+  // Функція створення налаштувань
+  getScrollWatcherConfig(paramsWatch) {
+    let configWatcher = {};
+    if (document.querySelector(paramsWatch.root)) {
+      configWatcher.root = document.querySelector(paramsWatch.root);
+    } else if (paramsWatch.root !== "null") ;
+    configWatcher.rootMargin = paramsWatch.margin;
+    if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
+      return;
+    }
+    if (paramsWatch.threshold === "prx") {
+      paramsWatch.threshold = [];
+      for (let i = 0; i <= 1; i += 5e-3) {
+        paramsWatch.threshold.push(i);
+      }
+    } else {
+      paramsWatch.threshold = paramsWatch.threshold.split(",");
+    }
+    configWatcher.threshold = paramsWatch.threshold;
+    return configWatcher;
+  }
+  // Функція створення нового спостерігача зі своїми налаштуваннями
+  scrollWatcherCreate(configWatcher) {
+    this.observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        this.scrollWatcherCallback(entry, observer);
+      });
+    }, configWatcher);
+  }
+  // Функція ініціалізації спостерігача зі своїми налаштуваннями
+  scrollWatcherInit(items, configWatcher) {
+    this.scrollWatcherCreate(configWatcher);
+    items.forEach((item) => this.observer.observe(item));
+  }
+  // Функція обробки базових дій точок спрацьовування
+  scrollWatcherIntersecting(entry, targetElement) {
+    if (entry.isIntersecting) {
+      !targetElement.classList.contains("--watcher-view") ? targetElement.classList.add("--watcher-view") : null;
+    } else {
+      targetElement.classList.contains("--watcher-view") ? targetElement.classList.remove("--watcher-view") : null;
+    }
+  }
+  // Функція відключення стеження за об'єктом
+  scrollWatcherOff(targetElement, observer) {
+    observer.unobserve(targetElement);
+  }
+  // Функція обробки спостереження
+  scrollWatcherCallback(entry, observer) {
+    const targetElement = entry.target;
+    this.scrollWatcherIntersecting(entry, targetElement);
+    targetElement.hasAttribute("data-fls-watcher-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
+    document.dispatchEvent(new CustomEvent("watcherCallback", {
+      detail: {
+        entry
+      }
+    }));
+  }
+}
+document.querySelector("[data-fls-watcher]") ? window.addEventListener("load", () => new ScrollWatcher({})) : null;
 function _assertThisInitialized(self) {
   if (self === void 0) {
     throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
@@ -7213,6 +7373,180 @@ gsapWithCSS.utils.toArray(".archive__item").forEach((item, index) => {
     }
   );
 });
+gsapWithCSS.fromTo(".info-project__image", {
+  scale: 0,
+  opacity: 0
+}, {
+  scrollTrigger: {
+    trigger: ".info-project",
+    start: "top center"
+  },
+  scale: 1,
+  opacity: 1,
+  duration: 1
+  // delay: 1, 
+});
+gsapWithCSS.fromTo(".info-project__title", {
+  y: 50,
+  opacity: 0
+}, {
+  scrollTrigger: {
+    trigger: ".info-project",
+    start: "top center"
+  },
+  y: 0,
+  opacity: 1,
+  duration: 1.3
+  // delay: 1, 
+});
+gsapWithCSS.fromTo(".info-project__period > *", {
+  y: 50,
+  opacity: 0
+}, {
+  scrollTrigger: {
+    trigger: ".info-project",
+    start: "top center"
+  },
+  y: 0,
+  opacity: 1,
+  stagger: 0.2,
+  duration: 0.8
+  // delay: 1, 
+});
+gsapWithCSS.fromTo(".info-project__about > *", {
+  y: 50,
+  opacity: 0
+}, {
+  scrollTrigger: {
+    trigger: ".info-project",
+    start: "top center"
+  },
+  y: 0,
+  opacity: 1,
+  stagger: 0.2,
+  duration: 0.8
+  // delay: 1, 
+});
+gsapWithCSS.fromTo(".info-project__partners > *", {
+  y: 50,
+  opacity: 0
+}, {
+  scrollTrigger: {
+    trigger: ".info-project",
+    start: "top center"
+  },
+  y: 0,
+  opacity: 1,
+  stagger: 0.2,
+  duration: 0.8
+  // delay: 1, 
+});
+gsapWithCSS.fromTo(".results__title", {
+  y: 50,
+  opacity: 0
+}, {
+  scrollTrigger: {
+    trigger: ".results",
+    start: "top center"
+  },
+  y: 0,
+  opacity: 1,
+  duration: 1
+  // delay: 1, 
+});
+gsapWithCSS.utils.toArray(".results__item").forEach((item, index) => {
+  gsapWithCSS.fromTo(
+    item,
+    {
+      y: 50,
+      opacity: 0
+    },
+    {
+      scrollTrigger: {
+        trigger: item,
+        start: "top-=150px center"
+      },
+      y: 0,
+      opacity: 1,
+      duration: 0.6,
+      delay: index * 0.2,
+      ease: "power2.out"
+    }
+  );
+});
+gsapWithCSS.utils.toArray(".info-block__text > *").forEach((item, index) => {
+  gsapWithCSS.fromTo(
+    item,
+    {
+      y: 50,
+      opacity: 0
+    },
+    {
+      scrollTrigger: {
+        trigger: item,
+        start: "top-=150px center"
+      },
+      y: 0,
+      opacity: 1,
+      duration: 0.6,
+      delay: index * 0.2,
+      ease: "power2.out"
+    }
+  );
+});
+gsapWithCSS.utils.toArray(".info-block__image").forEach((item, index) => {
+  gsapWithCSS.fromTo(
+    item,
+    {
+      y: 25,
+      opacity: 0
+    },
+    {
+      scrollTrigger: {
+        trigger: item,
+        start: "top-=150px center"
+      },
+      y: 0,
+      opacity: 1,
+      duration: 0.6,
+      delay: index * 0.2,
+      ease: "power2.out"
+    }
+  );
+});
+gsapWithCSS.fromTo(".about-us__title", {
+  y: 50,
+  opacity: 0
+}, {
+  scrollTrigger: {
+    trigger: ".about-us",
+    start: "top center"
+  },
+  y: 0,
+  opacity: 1,
+  duration: 1
+  // delay: 1, 
+});
+gsapWithCSS.utils.toArray(".about-us__article").forEach((item, index) => {
+  gsapWithCSS.fromTo(
+    item,
+    {
+      y: 25,
+      opacity: 0
+    },
+    {
+      scrollTrigger: {
+        trigger: item,
+        start: "top-=250px center"
+      },
+      y: 0,
+      opacity: 1,
+      duration: 0.6,
+      delay: index * 0.2,
+      ease: "power2.out"
+    }
+  );
+});
 function addLoadedClass() {
   if (!document.documentElement.hasAttribute("data-fls-preloader-loading")) {
     window.addEventListener("load", function() {
@@ -11925,163 +12259,3 @@ function initSliders() {
   }
 }
 document.querySelector("[data-fls-slider]") ? window.addEventListener("load", initSliders) : null;
-function digitsCounter() {
-  function digitsCountersInit(digitsCountersItems) {
-    let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-fls-digcounter]");
-    if (digitsCounters.length) {
-      digitsCounters.forEach((digitsCounter2) => {
-        if (digitsCounter2.hasAttribute("data-fls-digcounter-go")) return;
-        digitsCounter2.setAttribute("data-fls-digcounter-go", "");
-        digitsCounter2.dataset.flsDigcounter = digitsCounter2.innerHTML;
-        digitsCounter2.innerHTML = `0`;
-        digitsCountersAnimate(digitsCounter2);
-      });
-    }
-  }
-  function digitsCountersAnimate(digitsCounter2) {
-    let startTimestamp = null;
-    const duration = parseFloat(digitsCounter2.dataset.flsDigcounterSpeed) ? parseFloat(digitsCounter2.dataset.flsDigcounterSpeed) : 1e3;
-    const startValue = parseFloat(digitsCounter2.dataset.flsDigcounter);
-    const format = digitsCounter2.dataset.flsDigcounterFormat ? digitsCounter2.dataset.flsDigcounterFormat : " ";
-    const startPosition = 0;
-    const step = (timestamp) => {
-      if (!startTimestamp) startTimestamp = timestamp;
-      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-      const value = Math.floor(progress * (startPosition + startValue));
-      digitsCounter2.innerHTML = typeof digitsCounter2.dataset.flsDigcounterFormat !== "undefined" ? getDigFormat(value, format) : value;
-      if (progress < 1) {
-        window.requestAnimationFrame(step);
-      } else {
-        digitsCounter2.removeAttribute("data-fls-digcounter-go");
-      }
-    };
-    window.requestAnimationFrame(step);
-  }
-  function digitsCounterAction(e) {
-    const entry = e.detail.entry;
-    const targetElement = entry.target;
-    if (targetElement.querySelectorAll("[data-fls-digcounter]").length && entry.isIntersecting) {
-      digitsCountersInit(targetElement.querySelectorAll("[data-fls-digcounter]"));
-    }
-  }
-  document.addEventListener("watcherCallback", digitsCounterAction);
-}
-document.querySelector("[data-fls-digcounter]") ? window.addEventListener("load", digitsCounter) : null;
-class ScrollWatcher {
-  constructor(props) {
-    let defaultConfig = {
-      logging: true
-    };
-    this.config = Object.assign(defaultConfig, props);
-    this.observer;
-    !document.documentElement.classList.contains("watcher") ? this.scrollWatcherRun() : null;
-  }
-  // Оновлюємо конструктор
-  scrollWatcherUpdate() {
-    this.scrollWatcherRun();
-  }
-  // Запускаємо конструктор
-  scrollWatcherRun() {
-    document.documentElement.classList.add("watcher");
-    this.scrollWatcherConstructor(document.querySelectorAll("[data-fls-watcher]"));
-  }
-  // Конструктор спостерігачів
-  scrollWatcherConstructor(items) {
-    if (items.length) {
-      let uniqParams = uniqArray(Array.from(items).map(function(item) {
-        if (item.dataset.flsWatcher === "navigator" && !item.dataset.flsWatcherThreshold) {
-          let valueOfThreshold;
-          if (item.clientHeight > 2) {
-            valueOfThreshold = window.innerHeight / 2 / (item.clientHeight - 1);
-            if (valueOfThreshold > 1) {
-              valueOfThreshold = 1;
-            }
-          } else {
-            valueOfThreshold = 1;
-          }
-          item.setAttribute(
-            "data-fls-watcher-threshold",
-            valueOfThreshold.toFixed(2)
-          );
-        }
-        return `${item.dataset.flsWatcherRoot ? item.dataset.flsWatcherRoot : null}|${item.dataset.flsWatcherMargin ? item.dataset.flsWatcherMargin : "0px"}|${item.dataset.flsWatcherThreshold ? item.dataset.flsWatcherThreshold : 0}`;
-      }));
-      uniqParams.forEach((uniqParam) => {
-        let uniqParamArray = uniqParam.split("|");
-        let paramsWatch = {
-          root: uniqParamArray[0],
-          margin: uniqParamArray[1],
-          threshold: uniqParamArray[2]
-        };
-        let groupItems = Array.from(items).filter(function(item) {
-          let watchRoot = item.dataset.flsWatcherRoot ? item.dataset.flsWatcherRoot : null;
-          let watchMargin = item.dataset.flsWatcherMargin ? item.dataset.flsWatcherMargin : "0px";
-          let watchThreshold = item.dataset.flsWatcherThreshold ? item.dataset.flsWatcherThreshold : 0;
-          if (String(watchRoot) === paramsWatch.root && String(watchMargin) === paramsWatch.margin && String(watchThreshold) === paramsWatch.threshold) {
-            return item;
-          }
-        });
-        let configWatcher = this.getScrollWatcherConfig(paramsWatch);
-        this.scrollWatcherInit(groupItems, configWatcher);
-      });
-    }
-  }
-  // Функція створення налаштувань
-  getScrollWatcherConfig(paramsWatch) {
-    let configWatcher = {};
-    if (document.querySelector(paramsWatch.root)) {
-      configWatcher.root = document.querySelector(paramsWatch.root);
-    } else if (paramsWatch.root !== "null") ;
-    configWatcher.rootMargin = paramsWatch.margin;
-    if (paramsWatch.margin.indexOf("px") < 0 && paramsWatch.margin.indexOf("%") < 0) {
-      return;
-    }
-    if (paramsWatch.threshold === "prx") {
-      paramsWatch.threshold = [];
-      for (let i = 0; i <= 1; i += 5e-3) {
-        paramsWatch.threshold.push(i);
-      }
-    } else {
-      paramsWatch.threshold = paramsWatch.threshold.split(",");
-    }
-    configWatcher.threshold = paramsWatch.threshold;
-    return configWatcher;
-  }
-  // Функція створення нового спостерігача зі своїми налаштуваннями
-  scrollWatcherCreate(configWatcher) {
-    this.observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        this.scrollWatcherCallback(entry, observer);
-      });
-    }, configWatcher);
-  }
-  // Функція ініціалізації спостерігача зі своїми налаштуваннями
-  scrollWatcherInit(items, configWatcher) {
-    this.scrollWatcherCreate(configWatcher);
-    items.forEach((item) => this.observer.observe(item));
-  }
-  // Функція обробки базових дій точок спрацьовування
-  scrollWatcherIntersecting(entry, targetElement) {
-    if (entry.isIntersecting) {
-      !targetElement.classList.contains("--watcher-view") ? targetElement.classList.add("--watcher-view") : null;
-    } else {
-      targetElement.classList.contains("--watcher-view") ? targetElement.classList.remove("--watcher-view") : null;
-    }
-  }
-  // Функція відключення стеження за об'єктом
-  scrollWatcherOff(targetElement, observer) {
-    observer.unobserve(targetElement);
-  }
-  // Функція обробки спостереження
-  scrollWatcherCallback(entry, observer) {
-    const targetElement = entry.target;
-    this.scrollWatcherIntersecting(entry, targetElement);
-    targetElement.hasAttribute("data-fls-watcher-once") && entry.isIntersecting ? this.scrollWatcherOff(targetElement, observer) : null;
-    document.dispatchEvent(new CustomEvent("watcherCallback", {
-      detail: {
-        entry
-      }
-    }));
-  }
-}
-document.querySelector("[data-fls-watcher]") ? window.addEventListener("load", () => new ScrollWatcher({})) : null;
